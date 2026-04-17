e820a180fcc70fef445e80ad8bc69e61b46b0ed6
Mike Kold Hermann <mkh@cleanmanager.dk>
2026-04-17 16:12:09 +0200

    feat: simplify Denmark silhouette component structure

--- src/components/DenmarkSilhouette.tsx
+++ src/components/DenmarkSilhouette.tsx
@@ -2,16 +2,10 @@ import DenmarkSvg from "@/components/denmark";
 
 export default function DenmarkSilhouette() {
   return (
-    <div className="pointer-events-none fixed inset-0 z-0 overflow-hidden">
-      <DenmarkSvg
-        width="100vw"
-        height="100vh"
-        className="absolute top-0 left-0 h-full w-full opacity-[0.12] dark:opacity-[0.25]"
-      />
-
-      <div className="absolute right-2 bottom-2 font-mono text-xs text-blue-500 opacity-50">
-        {/* removed */}
-      </div>
-    </div>
+    <DenmarkSvg
+      width="100vw"
+      height="100vh"
+      className="fixed inset-0 opacity-[0.12] dark:opacity-[0.25]"
+    />
   );
 }
