20a4f294d976bbaffc150579ea9e8194a3122c94
Mike Kold Hermann <mkh@cleanmanager.dk>
2026-04-18 11:43:43 +0200

    feat: add pointer-events-none to DenmarkSilhouette
    Prevents interaction with the Denmark silhouette background

--- src/components/DenmarkSilhouette.tsx
+++ src/components/DenmarkSilhouette.tsx
@@ -5,7 +5,7 @@ export default function DenmarkSilhouette() {
     <DenmarkSvg
       width="100vw"
       height="100vh"
-      className="fixed inset-0 opacity-[0.12] dark:opacity-[0.25]"
+      className="pointer-events-none fixed inset-0 opacity-[0.12] dark:opacity-[0.25]"
     />
   );
 }
